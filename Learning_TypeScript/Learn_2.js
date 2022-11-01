7dfb44151d7e530a704c750c046bad2d95df8505
Growing-Jiwoo <fbaa4412@naver.com>
2022-11-01 21:09:51 +0900

    day02 learning

--- Learning_TypeScript/Learn_2.js
+++ Learning_TypeScript/Learn_2.js
@@ -1,50 +1,49 @@
-// function narrowExample (x: number|string){
-//     if(typeof x === 'string'){
-//         return console.log(x + '1')
-//     }
-//     else{
-//         return console.log(x + 1)
-//     }
-// }
-// narrowExample(123)
-// function narrowExample_2 (x: number|string){
-//     let array :number[] = [];
-//     array[0] = x as number;
-//     return array
-// }
-// narrowExample_2(123)
-// let array = [1,'2',3]
-// function narrowExample_3(x : number|string){
-//     array.push(x)
-//     return array
-// }
-// narrowExample_3(5)
-// function cleaningFunction_1(clean_array: (number|string)[]){
-//     let clean_array_result :number[] = [];
-//     clean_array.forEach((element) => {
-//         if (typeof(element) === 'string'){
-//             clean_array_result.push(parseInt(element))
-//         }
-//         else{
-//             clean_array_result.push(element)
-//         }
-//     });
-//     return console.log(clean_array_result)
-// }
-// function cleaningFunction_2(clean_array: (number|string)[]){
-//     clean_array.forEach((element,index) => {
-//         if (typeof(element) === 'string'){
-//             clean_array.splice(index,index,parseInt(element))
-//         }
-//     });
-//     return console.log(clean_array)
-// }
-// cleaningFunction_1(array)
-// cleaningFunction_2(array)
+function narrowExample(x) {
+    if (typeof x === 'string') {
+        return console.log(x + '1');
+    }
+    else {
+        return console.log(x + 1);
+    }
+}
+narrowExample(123);
+function narrowExample_2(x) {
+    let array = [];
+    array[0] = x;
+    return array;
+}
+narrowExample_2(123);
+let array = [1, '2', 3];
+function narrowExample_3(x) {
+    array.push(x);
+    return array;
+}
+narrowExample_3(5);
+function cleaningFunction_1(clean_array) {
+    let clean_array_result = [];
+    clean_array.forEach((element) => {
+        if (typeof (element) === 'string') {
+            clean_array_result.push(parseInt(element));
+        }
+        else {
+            clean_array_result.push(element);
+        }
+    });
+    return console.log(clean_array_result);
+}
+function cleaningFunction_2(clean_array) {
+    clean_array.forEach((element, index) => {
+        if (typeof (element) === 'string') {
+            clean_array.splice(index, index, parseInt(element));
+        }
+    });
+    return console.log(clean_array);
+}
+cleaningFunction_1(array);
+cleaningFunction_2(array);
 let teacher_1 = { subject: 'math' };
 let teacher_2 = { subject: ['science', 'english'] };
 let teacher_3 = { subject: ['science', 'art', 'korean'] };
-let teacher_4 = { hi: "hi" };
 function teacherSubject(array) {
     if (typeof (array.subject) === 'string') {
         return console.log(array.subject);
@@ -53,7 +52,8 @@ function teacherSubject(array) {
         return console.log(array[array.subject.length - 1]);
     }
     else {
-        return "잘못 지정된";
     }
 }
 teacherSubject({ subject: 'math' });
+teacherSubject({ subject: ['science', 'english'] });
+teacherSubject({ subject: ['science', 'art', 'korean'] });
--- Learning_TypeScript/Learn_2.ts
+++ Learning_TypeScript/Learn_2.ts
@@ -1,58 +1,57 @@
-// function narrowExample (x: number|string){
-//     if(typeof x === 'string'){
-//         return console.log(x + '1')
-//     }
-//     else{
-//         return console.log(x + 1)
-//     }
-// }
+function narrowExample (x: number|string){
+    if(typeof x === 'string'){
+        return console.log(x + '1')
+    }
+    else{
+        return console.log(x + 1)
+    }
+}
 
-// narrowExample(123)
+narrowExample(123)
 
-// function narrowExample_2 (x: number|string){
-//     let array :number[] = [];
-//     array[0] = x as number;
-//     return array
-// }
-// narrowExample_2(123)
+function narrowExample_2 (x: number|string){
+    let array :number[] = [];
+    array[0] = x as number;
+    return array
+}
+narrowExample_2(123)
 
-// let array = [1,'2',3]
-// function narrowExample_3(x : number|string){
-//     array.push(x)
-//     return array
-// }
+let array = [1,'2',3]
+function narrowExample_3(x : number|string){
+    array.push(x)
+    return array
+}
 
-// narrowExample_3(5)
+narrowExample_3(5)
 
-// function cleaningFunction_1(clean_array: (number|string)[]){
-//     let clean_array_result :number[] = [];
-//     clean_array.forEach((element) => {
-//         if (typeof(element) === 'string'){
-//             clean_array_result.push(parseInt(element))
-//         }
-//         else{
-//             clean_array_result.push(element)
-//         }
-//     });
-//     return console.log(clean_array_result)
-// }
+function cleaningFunction_1(clean_array: (number|string)[]){
+    let clean_array_result :number[] = [];
+    clean_array.forEach((element) => {
+        if (typeof(element) === 'string'){
+            clean_array_result.push(parseInt(element))
+        }
+        else{
+            clean_array_result.push(element)
+        }
+    });
+    return console.log(clean_array_result)
+}
 
-// function cleaningFunction_2(clean_array: (number|string)[]){
-//     clean_array.forEach((element,index) => {
-//         if (typeof(element) === 'string'){
-//             clean_array.splice(index,index,parseInt(element))
-//         }
-//     });
-//     return console.log(clean_array)
-// }
+function cleaningFunction_2(clean_array: (number|string)[]){
+    clean_array.forEach((element,index) => {
+        if (typeof(element) === 'string'){
+            clean_array.splice(index,index,parseInt(element))
+        }
+    });
+    return console.log(clean_array)
+}
 
-// cleaningFunction_1(array)
-// cleaningFunction_2(array)
+cleaningFunction_1(array)
+cleaningFunction_2(array)
 
 let teacher_1 = { subject : 'math' }
 let teacher_2 = { subject : ['science', 'english'] }
 let teacher_3 = { subject : ['science', 'art', 'korean'] }
-let teacher_4 = { hi : "hi"}
 
 function teacherSubject(array: {subject : string|string[]}){
 
@@ -63,9 +62,11 @@ function teacherSubject(array: {subject : string|string[]}){
         return console.log(array[array.subject.length-1])
     }
     else{
-        return "잘못 지정된"
+        
     }
     
 }
 
 teacherSubject({ subject : 'math' })
+teacherSubject({ subject : ['science', 'english'] })
+teacherSubject({ subject : ['science', 'art', 'korean'] })
